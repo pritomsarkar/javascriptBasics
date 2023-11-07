d9ff29ee6502213503a03041d5981fe597357916
Pritom Sarkar <43113813+pritomsarkar@users.noreply.github.com>
2023-11-07 16:15:57 +0000

    a1

---
+++ 01_basics/text.js
@@ -0,0 +1,26 @@
+const accountId = 12;
+let accountEmail = "Pritom";
+var accountPassword = "HeyU";
+accountCity = "Kanpur";
+
+
+// accountId = 2;
+accountEmail = "Pritom";
+accountPassword = "HeyU";
+accountCity = "Kanpur";
+
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
+// console.info(accountId)
+// var test = "12";
+var test1 = "12";
+let test = "3";
+
+
+{
+   test = "23";
+
+}
+
+console.log(test);
+console.log(test1);
